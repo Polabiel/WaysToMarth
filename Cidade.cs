d14611592cf89e618614e40dc008c2256c430c7b
u23600 <u23600@DINALVA12>
2024-03-25 19:59:04 -0300

    Adicionar arquivos de projeto.

---
+++ Cidade.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace apCaminhosEmMarte
+{
+  public class Cidade : IRegistro<Cidade>
+  {
+    // mapeamento da linha de dados do arquivo de cidades
+    const int
+      tamNome = 15,
+      tamX = 7,
+      tamY = 7,
+      inicioNome = 0,
+      inicioX = inicioNome + tamNome,
+      inicioY = inicioX + tamX;
+
+    string nomeCidade;
+    double x, y;
+
+    public string Chave => nomeCidade;
+
+    public void GravarDados(StreamWriter arquivo)
+    {
+      throw new NotImplementedException();
+    }
+
+    public void LerRegistro(StreamReader arquivo)
+    {
+      if (arquivo != null)  // arquivo foi aberto
+        if (! arquivo.EndOfStream) // se não acabou de ler
+        {
+          string linhaLida = arquivo.ReadLine();
+
+          // separamos cada campo a partir da linha lida
+          nomeCidade  = linhaLida.Substring(inicioNome, tamNome);
+          string strX = linhaLida.Substring(inicioX, tamX);
+          x = double.Parse(strX);
+          y = double.Parse(linhaLida.Substring(inicioY, tamY));
+        }
+    }
+  }
+}
---
+++ Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace apCaminhosEmMarte
+{
+  public partial class FrmCaminhos : Form
+  {
+    public FrmCaminhos()
+    {
+      InitializeComponent();
+    }
+
+    ITabelaDeHash<Cidade> tabela;
+
+    private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+    {
+
+    }
+
+    private void btnLerArquivo_Click(object sender, EventArgs e)
+    {
+      if (dlgAbrir.ShowDialog() == DialogResult.OK)
+      {
+        if (rbBucketHash.Checked)
+           tabela = new BucketHash<Cidade>();
+        else
+          if (rbHashLinear.Checked)
+             tabela = new HashLinear<Cidade>();
+          else 
+            if (rbHashQuadratico.Checked)
+               tabela = new HashQuadratico<Cidade>();
+            else
+              if (rbHashDuplo.Checked)
+                tabela = new HashDuplo<Cidade>();
+
+
+        var arquivo = new StreamReader(dlgAbrir.FileName);
+        while (! arquivo.EndOfStream) 
+        {
+          Cidade umaCidade = new Cidade();
+          umaCidade.LerRegistro(arquivo);
+          tabela.Inserir(umaCidade);
+        }
+
+        arquivo.Close();
+      }
+    }
+  }
+}
